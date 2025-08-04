81c878adfb4eefce83edb2178b6fe42287b0df0c
anumanual <anumanual1988@gmail.com>
2025-08-04 11:42:58 +0530

    login updated

--- server/src/main/java/com/instagram/auth/registration/RegisterRepository.java
+++ server/src/main/java/com/instagram/auth/registration/RegisterRepository.java
@@ -4,10 +4,9 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
-@EnableMongoRepositories
+//@EnableMongoRepositories
 public interface RegisterRepository  extends MongoRepository<Register, ObjectId>{
     Optional<Register> findByPhoneNumber(String phoneNumber);
     Optional<Register> findByUserName(String userName);

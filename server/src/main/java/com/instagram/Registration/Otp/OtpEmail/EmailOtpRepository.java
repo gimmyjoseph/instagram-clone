8c4f248b8fca92585d3511934492dd2abda7f2bf
gimmyjoseph <gimmyjimmygimmy@gmail.com>
2025-08-04 11:47:58 +0530

    solved otp issue

--- server/src/main/java/com/instagram/Registration/Otp/OtpEmail/EmailOtpRepository.java
+++ server/src/main/java/com/instagram/Registration/Otp/OtpEmail/EmailOtpRepository.java
@@ -1,5 +1,5 @@
 package com.instagram.Registration.Otp.OtpEmail;
-
+import org.bson.types.ObjectId;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -8,6 +8,6 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories
 
 
-public interface  EmailOtpRepository extends MongoRepository<EmailOtp, String>{
+public interface  EmailOtpRepository extends MongoRepository<EmailOtp, ObjectId>{
     List<EmailOtp> findByEmail(String email);
 }
